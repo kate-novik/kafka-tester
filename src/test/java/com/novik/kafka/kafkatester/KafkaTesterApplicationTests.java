1251d1862bea674edf5d4634a1da5c36bd1b7beb
Kate <ekat.novik@gmail.com>
2020-09-05 03:15:05 +0300

    Initiliaze git repository.

---
+++ src/test/java/com/novik/kafka/kafkatester/KafkaTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novik.kafka.kafkatester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
